8dd349c42aa8f77d2ba2c2153790017b2e102eac
Trent Huber <88948755+trenthuber@users.noreply.github.com>
2023-12-18 23:52:31 -0500

    Finished the structure of the cbs_rebuild system

--- cbs.c
+++ cbs.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <stdarg.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 typedef struct {
 	char **items;
@@ -42,21 +43,19 @@ void command_append(Command *command, char *string) {
 	strcpy(command->items[command->count++], string);
 }
 
-bool command_execute(Command command) {
-	// printf("ABOUT TO EXECUTE THIS:\n");
-	// command_print(command);
-	// command.items[command.count] = NULL;
-	// command_append(&command, NULL);
-	pid_t pid = fork();
-	if (pid == 0) { // Child process
-		if (execvp(command.items[0], command.items) == -1) {
-			perror("execve failed");
-			exit(1);
-		}
+#define command_multi_append(command, ...) command_multi_append_null_term(command, __VA_ARGS__, NULL)
+
+void command_multi_append_null_term(Command *command, ...) {
+	va_list args;
+	va_start(args, command);
+
+	char *next_arg = va_arg(args, char *);
+	while(next_arg) {
+		command_append(command, next_arg);
+		next_arg = va_arg(args, char *);
 	}
-	int status = 0;
-	wait(&status);
-	return status == 0;
+
+	va_end(args);
 }
 
 void command_clear(Command *command) {
@@ -69,7 +68,33 @@ void command_clear(Command *command) {
 	command->count = command->capacity = 0;
 }
 
-int main(int argc, char **argv) {
+bool command_execute(Command *command) {
+	pid_t pid = fork();
+	if (pid == 0) {
+		if (execvp(command->items[0], command->items) == -1) {
+			perror("execve failed");
+			exit(1);
+		}
+	}
+	int status = 0;
+	wait(&status);
+	command_clear(command);
+	return status == 0;
+}
+
+#define cbs_run(...) \
+	do { \
+		Command cmd = {0}; \
+		command_multi_append(&cmd, __VA_ARGS__); \
+		command_append(&cmd, NULL); \
+		if (!command_execute(&cmd)) { \
+			fprintf(stderr, "could not run command at %s(%u)\n", __FILE__, __LINE__); \
+			exit(1); \
+		}; \
+	} while(0)
+
+void cbs_rebuild(int argc, char **argv) {
+	(void) argc;
 	char *filename = argv[0];
 	char *filename_bak = calloc(strlen(filename) + 5, sizeof(char));
 	strcpy(filename_bak, filename);
@@ -77,80 +102,42 @@ int main(int argc, char **argv) {
 	char *filename_c = calloc(strlen(filename) + 3, sizeof(char));
 	strcpy(filename_c, filename);
 	strcat(filename_c, ".c");
-	// printf("filename_bak: %s, filename_c: %s\n", filename_bak, filename_c);
-	printf("Hello, seaman!\n");
 
 	// Check if we need to rebuild this file
-#ifndef _DARWIN_FEATURE_64_BIT_INODE
-#define _DARWIN_FEATURE_64_BIT_INODE
-#endif
 	struct stat my_stat;
 	stat(filename, &my_stat);
 	__darwin_time_t mtime = my_stat.st_mtime;
 	stat(filename_c, &my_stat);
 	__darwin_time_t c_mtime = my_stat.st_mtime;
 	if (mtime > c_mtime) {
-		return 0;
+		return;
 	}
 
 	Command cmd = {0};
-	command_append(&cmd, "cp");
-	command_append(&cmd, filename);
-	command_append(&cmd, filename_bak);
+	cbs_run("cp", filename, filename_bak);
+	command_multi_append(&cmd, "cc", "-o", filename, filename_c);
 	command_append(&cmd, NULL);
-	command_print(cmd);
-	if (!command_execute(cmd)) {
-		fprintf(stderr, "unable to create backup file");
-		exit(1);
-	}
-	command_clear(&cmd);
-	command_append(&cmd, "cc");
-	command_append(&cmd, "-o");
-	command_append(&cmd, filename);
-	command_append(&cmd, filename_c);
-	command_append(&cmd, NULL);
-	command_print(cmd);
-	if (!command_execute(cmd)) {
+	if (!command_execute(&cmd)) {
 		printf("Recompile unsuccessful\n");
-		command_clear(&cmd);
-		command_append(&cmd, "cp");
-		command_append(&cmd, filename_bak);
-		command_append(&cmd, filename);
-		command_append(&cmd, NULL);
-		if (!command_execute(cmd)) {
-			fprintf(stderr, "unable to copy backup file back");
-			exit(1);
-		}
-		command_clear(&cmd);
-		command_append(&cmd, "rm");
-		command_append(&cmd, "-f");
-		command_append(&cmd, filename_bak);
-		command_append(&cmd, NULL);
-		if (!command_execute(cmd)) {
-			fprintf(stderr, "unable to remove backup file");
-			exit(1);
-		}
-		return 1;
+		cbs_run("cp", filename_bak, filename);
+		cbs_run("rm", "-f", filename_bak);
+		exit(1);
 	} else {
 		printf("Recompile successful\n");
-		command_clear(&cmd);
-		command_append(&cmd, "rm");
-		command_append(&cmd, "-f");
-		command_append(&cmd, filename_bak);
+		cbs_run("rm", "-f", filename_bak);
+		command_multi_append(&cmd, filename);
 		command_append(&cmd, NULL);
-		if (!command_execute(cmd)) {
-			fprintf(stderr, "unable to remove backup file");
-			exit(1);
-		}
-		command_clear(&cmd);
-		command_append(&cmd, filename);
-		command_append(&cmd, NULL);
-		command_print(cmd);
 		if (execvp(cmd.items[0], cmd.items) == -1) {
 			perror("could not rebuild project");
 			exit(1);
 		}
 	}
-	
+}
+
+int main(int argc, char **argv) {
+	cbs_rebuild(argc, argv);
+
+	printf("Hello, world!\n");
+
 	return 0;
 }
