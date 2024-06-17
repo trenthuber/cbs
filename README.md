812b094e3dcc18b5e73c995fb5b95afe78b874f3
Trent Huber <trentmhuber@gmail.com>
2024-06-17 02:07:11 -0400

    Major update with many changes
     - Fixed bug in getting program return status (cbs_cmd_run_status() and
       cbs_async_wait())
     - Wrote an interface overview for cbs.h to simplify readability and usability
     - Removed LICENSE (added it to cbs.h) and cbs_template.c (added it to
       README.md)
     - Heavily simplified README.md
     - Deleted "clear" and "free" functions (practically speaking, they're useless)

--- README.md
+++ README.md
@@ -1,61 +1,36 @@
-# cbs, a C build system
+# cbs
 
-## Motivation
-The idea for this project is directly stolen from a recreational programming streamer, [Tsoding Daily](https://www.youtube.com/watch?v=eRt7vhosgKE&list=PLpM-Dvs8t0Va1sCJpPFjs2lKzv8bw3Lpw), specifically from his [musializer](https://github.com/tsoding/musializer) project.
+## Summary
+cbs is a build system for C written in C (although it can certainly be used to build more than just C projects). While most modern day build systems are far more feature rich than this project, they lack at least one thing: a fully developed and mature build language.
 
-**As it stands:** Practically speaking, building a complex C project requires a build system to build it. These build systems, such as Make or CMake, all depend on other pieces of software you need preinstalled to your computer before you can even start the build process. But why should a C project depend on anything besides a C compiler? I mean, for as "low-level" as we call C, shouldn't we be able to do everything we desire with only a C compiler?
+That's where cbs comes in.
 
-**As it *should* stand:** A C project should be able to build using only a C compiler. This means replacing any previously used build systems with a system that is essentially just a C library and replacing our precious build scripts with C source code that uses that C library. In this way, we not only ditch any build dependency, but we also have the luxury (some might think it far from it) of being able to write our build script in the same language we wrote the rest of our project in! No need to learn how to write a Makefile or a CMakeList.txt, we can write it all in one lanaguage.
+In essence, build scripts written with cbs are only constrained by the capabilities of the C programming language itself, not some superficially created one. While perhaps more verbose than others, C's capabilites as a "real" and fully-fleshed out programming langauge make it worth the effort in the end. To make the process that much easier, much of the functionality that is common in building projects has been wrapped up in this single-file library, including running Bourne shell commands, navigating file trees, extracting names and extensions of files, and even multi-process building (similar to the -j option in Make).
 
-This idea can technically be applied (and perhaps applied easier than it is here) to any other language, but it seems fitting for it to at least first be tried in C given how low-level C projects already are.
+## Getting Started
+To use the library, simply create a `cbs.c` file and include `cbs.h`. Similar to other single-file libraries, you can include the actual implementation by defining the `CBS_IMPLEMENTATION` macro. Common boilerplate for cbs looks like:
+```c
+#define CBS_IMPLEMENTATION
+#include "cbs.h"
 
-## Obstacles
-In order for this library to actually solve our problem, we can't have it be something you need to install in order to use. So, it's necessary that we go with a [single-file library](https://github.com/nothings/single_file_libs) structure for this build system project. All you need to do then is copy a *single file* into your project and create a build script in C. Additionally, since the build system is entirely contained inside the project, anyone that builds it for themselves only needs a C compiler. It's an inherently portable solution.
+int main(int argc, char **argv) {
+	cbs_rebuild_self(argv);
+	cbs_shift_args(&argc, &argv);
 
-Using C source code as a build script means that we would need to compile the code before we run it. This is fine if we never plan on changing the build script but realistically, this would mean having to recompile the build script source code every time we wanted to build our project. This is suboptimal, but cleverly solved by having the previous build system executable *rebuild itself* every time the source code is updated (see [File caching](#file_caching)). This requires putting the `cbs_rebuild_self` function at the top of the build script.
+	// Build script goes here
 
-## <a name="build"></a>Build
-Make sure to copy the `cbs.h` file to project and in the **same directory** make a `cbs.c` file with the build script (an [example template](./cbs_template.c) has been included).
-
-The **first time** you build your project, you'll need to [bootstrap](#bootstrap) the build system executable as such:
-
-```console
-$ cc -o cbs cbs.c
-$ ./cbs
+	return 0;
+}
 ```
 
-Every time after that you **don't need to bootstrap again**, and can just run
-
+To run your build program initially, you must first bootstrap it. Simply run:
 ```console
-$ ./cbs
+cc -o cbs cbs.c && ./cbs # Run this the first time
 ```
 
-cbs has functions that make parsing command line options easy. Using them, you could run your own defined subcommands. For example:
-
+Every time after that, the `cbs_rebuild_self()` function (assuming you put it at the top of your program) will handle rebuilding the `cbs.c` program and rerunning for you. Thus, to build your project after the initial bootstrapping, you only need to run:
 ```console
-$ ./cbs build
-$ ./cbs run
-$ ./cbs clean
+./cbs # Run this every time after
 ```
 
-## Features
-### <a name="bootstrap"></a>Automatic build system rebuilding
-As previously mentioned, you can include a function call at the top of the build script and it will make the build system recompile its own build script every time it runs. This means running your build system executable will handle everything for you, there is no need to manually recompile your build script every time.
-
-#### Note 1:
-When you first create you build script there is no initial build executable to automatically rebuild, so you must first *bootstrap* the executable by compiling the build script yourself (see [Build](#build)). This step is *only done once* as an initialization step. Once you've generated the executable, you shouldn't need to bootstrap again.
-
-#### Note 2:
-When using cbs, you must include a file named `cbs.c` in the same directory as you have `cbs.h` in, and the executable generated must also be originally bootstrapped in the same directory. These are necessary requirements for the rebuild feature to work properly. The generated build system executable will have the same name as the executable you ran to build it, so, for the sake of habit, you can name the originally bootstrapped executable anything you wish and that name will persist.
-
-### <a name="file_caching"></a>File caching
-Make looks at the modification time of the dependencies of each recipe and compares them to the modification time of the target. If any dependencies were modified after the target, it executes the recipe. It is literally as simple as it sounds. cbs can do the exact same thing with functions that check if targets need to be rebuilt. cbs uses this feature when rebuilding itself. 
-
-### Multi-process execution
-Larger projects can become quite slow to build, especially if multiple dependencies need to be recompiled inside the project. Many build systems offer the option of "multithreading" the build process, which really just means running different sections of the build script in separate child processes. cbs offers similar functionality except the user has *direct control* over which commands get run asynchronously and which don't.
-
-### File path pattern matching
-Often times you want to run the same set of commands on multiple files in your project. Make offers the ability to pattern match target names to dependency names and then use both names in the recipe to avoid redundancy. cbs has a similar functionality in which you can search in certain directories for file paths ending in a specifed file extension and get back a dynamic array of their file paths which you can then use in building your commands. In some ways, this is far more intuitive than the approach make takes, even if it's a little more verbose.
-
-## License
-See [LICENSE](./LICENSE) file for license rights and limitations.
+See `cbs.h` for an overview on the interface.
--- cbs.h
+++ cbs.h
@@ -1,3 +1,111 @@
+/* cbs, Trent Huber 2024
+ *
+ *
+ * SUMMARY
+ *
+ * cbs is a build system for C written in C (although it can certainly be used
+ * to build more than just C projects). While most modern day build systems are
+ * far more feature rich than this project, they lack at least one thing: a
+ * fully developed and mature build language. That's where cbs comes in. In
+ * essence, build scripts written with cbs are only constrained by the
+ * capabilities of the C programming language itself, not some superficially
+ * created one. While perhaps more verbose than others, C's capabilites as a
+ * "real" and fully-fleshed out programming langauge make it worth the effort in
+ * the end. To make the process that much easier, much of the functionality that
+ * is common in building projects has been wrapped up in this single-file
+ * library, including running Bourne shell commands, navigating file trees,
+ * extracting names and extensions of files, and even multi-process building
+ * (similar to the -j option in Make).
+ *
+ * See README.md for details on getting started.
+ *
+ *
+ * INTERFACE OVERVIEW
+ *
+ * Here is a list of all the functions available to you. Note that the actual
+ * implementation details are not important here (i.e., some of these
+ * "functions" are really macros, but that doesn't matter much). They've been
+ * organized in the order they would be "typically" used in a build program.
+ *
+ * RET    NAME                          PARAMETERS [^]
+ *
+ * - Self rebuilding -
+ * void   cbs_rebuild_self              (char *const *argv);
+ *
+ * - General purpose -
+ * void   cbs_log                       (const char *msg);
+ * void   cbs_error                     (const char *msg);
+ * char * cbs_shift_args                (int *argc_p, char ***argv_p);
+ * bool   cbs_string_eq                 (const char *string1,
+ *                                       const char *string2);
+ * char * cbs_string_build              (const char *string, ...);
+ * char * cbs_get_file_name             (const char *file_path);
+ * char * cbs_strip_file_ext            (const char *file_path);
+ * void   cbs_cd                        (const char *dir);
+ *
+ * - File paths -
+ * void   cbs_file_paths_append         (Cbs_File_Paths *file_paths,
+ *                                       Cbs_File_Path file_path);
+ * void   cbs_file_paths_build          (Cbs_File_Paths *file_paths,
+ *                                       Cbs_File_Path file_path, ...);
+ * void   cbs_file_paths_build_file_ext (Cbs_File_Paths *file_paths,
+ *                                       const char *dir_path, const char *ext);
+ * void   cbs_file_paths_for_each       (Cbs_File_Path file_path,
+ *                                       Cbs_File_Paths *file_paths);
+ *
+ * - File caching -
+ * bool   cbs_file_exists               (const char *file_path);
+ * bool   cbs_files_exist               (const char *file_path, ...);
+ * bool   cbs_needs_rebuild             (const char *target, const char *dep,
+ *                                       ...);
+ * bool   cbs_needs_rebuild_file_paths  (const char *target,
+ *                                       Cbs_File_Paths deps);
+ *
+ * - Build and run commands -
+ * void   cbs_cmd_append                (Cbs_Cmd *cmd, const char *string);
+ * void   cbs_cmd_build                 (Cbs_Cmd *cmd, const char *string, ...);
+ * void   cbs_cmd_build_file_paths      (Cbs_Cmd *cmd,
+ *                                       Cbs_File_Paths file_paths);
+ * void   cbs_cmd_print                 (Cbs_Cmd *cmd);
+ * int    cbs_cmd_run_status            (Cbs_Cmd *cmd);
+ * void   cbs_cmd_run                   (Cbs_Cmd *cmd);
+ *
+ * - Run commands directly -
+ * int    cbs_run_status                (const char *string, ...);
+ * void   cbs_run                       (const char *string, ...);
+ *
+ * - Run commands asynchronously -
+ * void   cbs_cmd_async_run             (Cbs_Async_Procs *procs, Cbs_Cmd *cmd);
+ * void   cbs_async_run                 (Cbs_Async_Procs *procs,
+ *                                       const char *string, ...);
+ * void   cbs_async_wait                (Cbs_Async_Procs *procs);
+ *
+ * [^] For functions whose parameter list ends in an ellipsis, this represents that the function takes variatic arguments of the same type as the parameter preceding the ellipsis.
+ *
+ *
+ * LICENSE
+ *
+ * Copyright (c) 2024 Trent Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef _CBS_H_
 #define _CBS_H_
 
@@ -5,16 +113,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-// cbs logging
-#define cbs_log(x) printf("%s\n", x)
-#define cbs_error(x) \
-	do { \
-		fprintf(stderr, "ERROR: %s (%s:%u)\n", x, __FILE__, __LINE__); \
-		exit(errno ? (perror("INFO"), errno) : EXIT_FAILURE); \
-	} while(0)
+// Internal macros used for creating dynamic arrays
+#define cbs__da_init_cap 16
 #define cbs__malloc_error cbs_error("Process ran out of memory")
-
-// Useful macros for generic dynamic arrays used in cbs data structures
 #define cbs__da_struct(Type, Name) \
 	typedef struct { \
 		Type *items; \
@@ -24,7 +125,7 @@
 #define cbs__append_item(da, item) \
 	do { \
 		if ((da)->cap == 0) { \
-			(da)->cap = 1; \
+			(da)->cap = cbs__da_init_cap; \
 			if (((da)->items = malloc(sizeof(*(da)->items))) == NULL) \
 				cbs__malloc_error; \
 		} else if ((da)->count >= (da)->cap) { \
@@ -51,22 +152,31 @@
 #define cbs__append_items(Type, da, ...) \
 	cbs__append_list(da, ((Type[]) {__VA_ARGS__}), \
 	                 (size_t) (sizeof((Type[]) {__VA_ARGS__}) / sizeof(Type)))
-#define cbs__clear(da) \
-	do { \
-		if ((da)->items) free((da)->items); \
-		(da)->items = NULL; \
-		(da)->count = (da)->cap = 0; \
-	} while(0)
+#define cbs__clear(da) (da)->count = 0
 
+// Self rebuilding: running "./cbs" rebuilds "cbs.c" automatically
 void cbs_rebuild_self(char *const *argv);
 
-// Utility functions: parsing command line options and manipulating file paths
+// Utility functions: logging, parsing the command line, modifying file paths
+#define cbs_log(x) printf("%s\n", x)
+#define cbs_error(x) \
+	do { \
+		fprintf(stderr, "ERROR: %s (%s:%u)\n", x, __FILE__, __LINE__); \
+		exit(errno ? (perror("INFO"), errno) : EXIT_FAILURE); \
+	} while(0)
 char *cbs_shift_args(int *argc_p, char ***argv_p);
-#define cbs_string_eq(str1, str2) strcmp(str1, str2) == 0
+#define cbs_string_eq(string1, string2) strcmp(string1, string2) == 0
 #define cbs_string_build(string, ...) \
 	cbs__string_build_nt(string, __VA_ARGS__, NULL)
 char *cbs_get_file_name(const char *file_path);
 char *cbs_strip_file_ext(const char *file_path);
+#define cbs_cd(dir) \
+	do { \
+		chdir(dir); \
+		Cbs_Cmd cmd = {0}; \
+		cbs_cmd_build(&cmd, "cd", dir); \
+		cbs_cmd_print(cmd); \
+	} while(0)
 
 // File paths
 cbs__da_struct(const char *, Cbs_File_Paths);
@@ -74,8 +184,8 @@ cbs__da_struct(const char *, Cbs_File_Paths);
 	cbs__append_item(file_paths, file_path)
 #define cbs_file_paths_build(file_paths, ...) \
 	cbs__append_items(const char *, file_paths, __VA_ARGS__)
-void cbs_file_paths_build_file_ext(Cbs_File_Paths *file_paths, 
-                                   const char *dir_path, 
+void cbs_file_paths_build_file_ext(Cbs_File_Paths *file_paths,
+                                   const char *dir_path,
                                    const char *ext);
 #define cbs_file_paths_for_each(file_path, file_paths) \
 	for (const char *file_path = file_paths.items[0], \
@@ -83,10 +193,8 @@ void cbs_file_paths_build_file_ext(Cbs_File_Paths *file_paths,
 	     **end = &file_paths.items[file_paths.count - 1]; \
 	     current <= end; \
 	     ++current, file_path = *current)
-#define cbs_file_paths_clear(file_paths) cbs__clear(file_paths)
-#define cbs_file_paths_free(...) cbs__file_paths_free_nt(__VA_ARGS__, NULL)
 
-// Useful conditionals, i.e., file caching
+// File caching conditionals
 bool cbs_file_exists(const char *file_path);
 #define cbs_files_exist(file_path, ...) \
 	cbs__files_exist_nt(file_path, __VA_ARGS__, NULL)
@@ -106,12 +214,13 @@ void cbs_cmd_build_file_paths(Cbs_Cmd *cmd, Cbs_File_Paths file_paths);
 		for (size_t i = 0; i < (cmd).count - 1; ++i) printf("%s ", (cmd).items[i]); \
 		printf("%s\n", (cmd).items[(cmd).count - 1]); \
 	} while(0)
-#define cbs_cmd_clear(cmd) cbs__clear(cmd)
-bool cbs_cmd_try_run(Cbs_Cmd *cmd);
-void cbs_cmd_run(Cbs_Cmd *cmd);
+int cbs_cmd_run_status(Cbs_Cmd *cmd);
+#define cbs_cmd_run(cmd) \
+	if (cbs_cmd_run_status(cmd)) \
+		cbs_error("Previous command ran unsuccessfully, stopping build");
 
 // Run commands directly
-#define cbs_try_run(...) cbs__try_run_nt(__VA_ARGS__, NULL)
+#define cbs_run_status(...) cbs__run_status_nt(__VA_ARGS__, NULL)
 #define cbs_run(...) \
 	do { \
 		Cbs_Cmd cmd = {0}; \
@@ -198,7 +307,7 @@ char *cbs_strip_file_ext(const char *file_path) {
 	return result;
 }
 
-static bool cbs__file_has_ext(const char *file_path, const char *ext) {
+bool cbs__file_has_ext(const char *file_path, const char *ext) {
 	size_t file_len = strlen(file_path), ext_len = strlen(ext);
 	if (file_len <= ext_len) return false;
 	for (--file_len, --ext_len; ext_len > 0; --file_len, --ext_len)
@@ -226,21 +335,6 @@ void cbs_file_paths_build_file_ext(Cbs_File_Paths *file_paths,
 		entry = readdir(dir);
 	}
 	if (closedir(dir) == -1) cbs_error("Unable to close directory after search");
-}
-
-void cbs__file_paths_free_nt(Cbs_File_Paths *file_paths, ...) {
-	for (size_t i = 0; i < file_paths->count; ++i)
-		free((char *) file_paths->items[i]);
-	cbs_file_paths_clear(file_paths);
-
-	va_list args;
-	va_start(args, file_paths);
-	Cbs_File_Paths *arg;
-	while ((arg = va_arg(args, Cbs_File_Paths *))) {
-		for (size_t i = 0; i < arg->count; ++i) free((char *) arg->items[i]);
-		cbs_file_paths_clear(arg);
-	}
-	va_end(args);
 }
 
 bool cbs_file_exists(const char *file_path) {
@@ -311,53 +405,38 @@ void cbs_cmd_build_file_paths(Cbs_Cmd *cmd, Cbs_File_Paths file_paths) {
 		cbs_file_paths_for_each(file_path, file_paths) cbs_cmd_append(cmd, file_path);
 }
 
-#define cbs__cmd_run_save_status(cmd) \
-	do { \
-		cbs_cmd_print(*cmd); \
-		cbs_cmd_append(cmd, NULL); \
-		pid_t child_pid; \
-		if ((child_pid = fork()) == 0 \
-		    && (execvp(cmd->items[0], (char *const *) cmd->items) == -1)) \
-			cbs_error("Unable to run command, check command syntax"); \
-		waitpid(child_pid, &status, 0); \
-		cbs_cmd_clear(cmd); \
-	} while(0)
-
-void cbs_cmd_run(Cbs_Cmd *cmd) {
-	int status = 0;
-	cbs__cmd_run_save_status(cmd);
-	if (status) cbs_error("Previous command ran unsuccessfully, stopping build");
+int cbs_cmd_run_status(Cbs_Cmd *cmd) {
+	cbs_cmd_print(*cmd);
+	cbs_cmd_append(cmd, NULL);
+	pid_t child_pid;
+	if ((child_pid = fork()) == 0 &&
+	    (execvp(cmd->items[0], (char *const *) cmd->items) == -1))
+		cbs_error("Unable to run command, check command syntax");
+	int stat, status;
+	waitpid(child_pid, &stat, 0);
+	if (WIFEXITED(stat)) status = WEXITSTATUS(stat);
+	else status = -1;
+	cbs__clear(cmd);
+	return status;
 }
 
-bool cbs_cmd_try_run(Cbs_Cmd *cmd) {
-	int status = 0;
-	cbs__cmd_run_save_status(cmd);
-	if (status) cbs_log("Previous command ran unsuccessfully, continuing build");
-	return !status;
-}
-
-#define cbs__append_from_va(da, Type, first) \
-	do { \
-		cbs__append_item(&da, first); \
-		va_list args; \
-		va_start(args, first); \
-		Type arg; \
-		while ((arg = va_arg(args, Type))) cbs__append_item(&da, arg); \
-		va_end(args); \
-	} while(0)
-
-bool cbs__try_run_nt(const char *string, ...) {
+int cbs__run_status_nt(const char *string, ...) {
 	Cbs_Cmd cmd = {0};
-	cbs__append_from_va(cmd, const char *, string);
-	return cbs_cmd_try_run(&cmd);
+	cbs__append_item(&cmd, string);
+	va_list args;
+	va_start(args, string);
+	const char *arg;
+	while ((arg = va_arg(args, const char *))) cbs__append_item(&cmd, arg);
+	va_end(args);
+	return cbs_cmd_run_status(&cmd);
 }
 
 void cbs_rebuild_self(char *const *argv) {
 	const char *this_file_name = argv[0];
 	if (!cbs_needs_rebuild(this_file_name, "cbs.c", "cbs.h")) return;
 	cbs_log("Rebuilding cbs");
-	if (!cbs_try_run("cc", "-Wall", "-Wextra", "-Wpedantic",
-	                 "-o", this_file_name, "cbs.c"))
+	if (cbs_run_status("cc", "-Wall", "-Wextra", "-Wpedantic",
+	                   "-o", this_file_name, "cbs.c"))
 		cbs_error("Rebuild unsuccessful, bootstrapping may be necessary");
 	cbs_log("Rebuild successful");
 	if (execvp(this_file_name, argv) == -1)
@@ -382,11 +461,11 @@ void cbs_cmd_async_run(Cbs_Async_Procs *procs, Cbs_Cmd *cmd) {
 			cbs_error("Unable to run command, check command syntax");
 	}
 
-	cbs_cmd_clear(cmd);
+	cbs__clear(cmd);
 	cbs__append_item(procs, proc);
 }
 
-static void cbs__file_print(FILE *file, FILE *output) { 
+void cbs__file_print(FILE *file, FILE *output) {
 	if (fseek(file, 0, SEEK_END) == -1)
 		cbs_error("Unable to seek file during print");
 	long file_size = ftell(file);
@@ -412,10 +491,12 @@ static void cbs__file_print(FILE *file, FILE *output) {
 
 void cbs_async_wait(Cbs_Async_Procs *procs) {
 	if (procs == NULL || procs->count == 0) return;
-	int status = 0;
+	int stat, status;
 	for (size_t i = 0; i < procs->count; ++i) {
 		Cbs__Async_Proc proc = procs->items[i];
-		waitpid(proc.pid, &status, 0);
+		waitpid(proc.pid, &stat, 0);
+		if (WIFEXITED(stat)) status = WEXITSTATUS(stat);
+		else status = -1;
 		cbs_cmd_print(proc.cmd);
 		cbs__file_print(proc.output, status ? stderr : stdout);
 		if (status) cbs_error("Previous command ran unsuccessfully, stopping build");
